b97ce5772ffe7bec50f1b9ddc367a504f41e0ea7
TinNguyen <ductin16111@gmail.com>
2023-11-28 02:06:53 +0200

    create fake RestAPI server and fetch tasks from it

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 function App() {
     const [isFormOpen, setIsFormOpen] = useState(false)
-    const [tasks, setTasks] = useState([
-        { id: 1, text: 'Doctors Appointment', day: 'Feb 5th 2:30pm', reminder: true },
-        { id: 2, text: 'Meeting at School', day: 'Feb 6th 1:30pm', reminder: false },
-        { id: 3, text: 'Food shopping', day: 'Feb 5th 2:30pm', reminder: true },
-    ])
+    const [tasks, setTasks] = useState([])
+
+    useEffect(() => {
+        const getTasks = async () => {
+            const initialTasksFromServer = await fetchTasks()
+            setTasks(initialTasksFromServer)
+        }
+        getTasks()
+    }, [])
+
+    // Fetch Tasks from mock API server
+    const fetchTasks = async () => {
+        const res = await fetch('http://localhost:5000/tasks')
+        const data = await res.json()
+        return data
+    }
 
     // Delete Task
     const deleteTask = (id) => {
